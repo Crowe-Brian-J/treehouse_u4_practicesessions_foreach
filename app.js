0a676149d7513361b4624f470da872ae0e68ac1e
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-06 22:48:55 -0400

    Challenge Task 2 complete

--- app.js
+++ app.js
@@ -15,3 +15,29 @@ numbers.forEach((number) => (total += number)) //Expected Result: 15
 
 //So I can see the answer below
 console.log(total)
+
+/*
+forEach Chanllenge Task 2
+
+Directions:
+Using forEach, copy only the first 2 characters of each string in the days array and store the abbreviations in the dayAbbreviations array.
+
+Given code below:
+*/
+
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+]
+let dayAbbreviations = []
+
+// dayAbbreviations should be: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa']
+// Write your code below
+days.forEach((day) => dayAbbreviations.push(day.substring(0, 2)))
+
+console.log(dayAbbreviations)
