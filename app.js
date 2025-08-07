1a5521a0de1b006624eb79990321c07284f698d1
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-06 23:01:10 -0400

    Challenge Task 5 Complete

--- app.js
+++ app.js
@@ -81,3 +81,25 @@ alphabet.forEach((letter) => {
 })
 
 console.log(noel)
+
+/*
+forEach Chanllenge Task 5
+
+Directions:
+Using forEach, iterate over the percentages and store all percentages over 50 percent in the array upperRange.
+
+Given code below:
+*/
+
+const percentages = [34, 67, 12, 39, 90, 82, 22, 24, 99]
+let upperRange = []
+
+// upperRange should be: [67, 90, 82, 99]
+// Write your code below
+percentages.forEach((percentage) => {
+  if (percentage > 50) {
+    upperRange.push(percentage)
+  }
+})
+
+console.log(upperRange)
