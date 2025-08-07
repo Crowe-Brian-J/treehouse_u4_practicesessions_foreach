e1daaa16051938b9166753b67524963a4cd3be24
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-06 22:45:26 -0400

    initial file setup and Challenge Task 1 complete

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # treehouse_u4_practicesessions_foreach
+I am maintaining this as a way to show my work through github as team treehouse uses an internal monitor for progress instead of commits.
---
+++ app.js
@@ -0,0 +1,17 @@
+/*
+forEach Chanllenge Task 1
+
+Directions:
+Using forEach, add together all the elements in the numbers array, and store the total in the variable total.
+
+Given code below:
+*/
+
+const numbers = [1, 2, 3, 4, 5]
+let total = 0
+
+// Write loop here:
+numbers.forEach((number) => (total += number)) //Expected Result: 15
+
+//So I can see the answer below
+console.log(total)
